fa879ccb0c9ba61cf68206564d56d67f402ce65a
BluAtlas <dciandersn@gmail.com>
2023-06-24 17:36:46 -0600

    removed hello world

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,9 @@
   </p>
 
   <CardViewer :searchData="searchData"></CardViewer>
-
-  <!--img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App" /-->
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 import NavBar from './components/NavBar.vue';
 import CardViewer from './components/CardViewer.vue';
 
