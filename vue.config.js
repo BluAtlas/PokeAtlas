119dc5b6ae16a0383d4ab634df312dabdc3b085c
BluAtlas <dciandersn@gmail.com>
2023-06-24 17:53:45 -0600

    different filename hash system

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  configureWebpack: { output: { chunkFilename: 'js/[name].js?id=[chunkhash]' } },
+  configureWebpack: { output: { filename: '[name].[fullhash].bundle.js' } },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/PokeAtlas/" : "/",
 })
