914792198330d6d0ce4ad61c34f8e5fc5ede7b4f
BluAtlas <dciandersn@gmail.com>
2023-06-24 17:44:43 -0600

    testing different webpack settings

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: { output: { chunkFilename: 'js/[name].js?id=[chunkhash]' } },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/PokeAtlas/" : "/",
 })
