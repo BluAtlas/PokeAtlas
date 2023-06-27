123bb0234129eac249199f37142e54c57f64f089
BluAtlas <dciandersn@gmail.com>
2023-06-26 18:31:10 -0600

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  configureWebpack: { output: { filename: '[name].[fullhash].bundle.js' } },
+  //configureWebpack: { output: { filename: '[name].[fullhash].bundle.js' } },
+  configureWebpack: { output: { chunkFilename: 'js/[name].js?id=[chunkhash]' } },
+
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/PokeAtlas/" : "/",
 })
